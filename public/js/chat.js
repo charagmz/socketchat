95ff79b3250d51bf245ba7a8ea66423e93860cb2
Charly Aguirre <charly.aguirre@pacific-hs.com>
2022-06-16 14:12:09 -0500

    Validacion JWT

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,52 @@
-const socket = io();
+const url = (window.location.hostname.includes('localhost')) 
+            ? 'http://localhost:4000/api/auth/' 
+            : 'https://restservernjs.herokuapp.com/api/auth/';
+
+let usuario = null;
+let socket  = null;
+
+// Validar el token del localstorage
+const validarJWT = async () => {
+
+    const token = localStorage.getItem('token') || '';
+    if (token.length <= 10) {
+        window.location = 'index.html';
+        throw new Error('No hay token en el servidor');
+    }
+
+    try {
+        const resp = await fetch (url, {
+            headers: {'x-token': token}
+        });
+
+        const {usuario: userDB, token: tokenDB} = await resp.json();
+        // renovar el token
+        localStorage.setItem('token', tokenDB);
+        usuario = userDB;
+        document.title = usuario.nombre;
+        
+        await conectarSocket();
+    } catch (error) {
+        window.location = 'index.html';
+        throw new Error('Token no valido');
+    }
+}
+
+const conectarSocket = async () => {
+
+    const socket = io({
+        'extraHeaders': {
+            'x-token': localStorage.getItem('token')
+        }
+    });
+}
+
+
+const main = async () => {
+
+    await validarJWT();
+}
+
+main();
+
+//
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,6 +1,10 @@
+const {Socket} = require('socket.io');
 
-const socketController = (socket) => {
-    console.log('cliente conectado', socket.id);
+
+const socketController = (socket = new Socket()) => {
+    //console.log('cliente conectado', socket.id);
+
+    console.log(socket.handshake.headers['x-token']);
 }
 
 module.exports = {
